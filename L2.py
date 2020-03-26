46a43ae5c5862f287e5118238791a0b21834ac60
Szkodzik <janekszkoda@gmail.com>
2020-03-26 11:54:00 +0100

    add L2

---
+++ L2.py
@@ -0,0 +1,9 @@
+from random import *
+from copy import *
+
+
+lista = []
+dlugosc_listy = 100
+for i in range(dlugosc_listy):
+    lista.append(randint(-100, 100))
+print("Lista do sortowania: ",lista)
